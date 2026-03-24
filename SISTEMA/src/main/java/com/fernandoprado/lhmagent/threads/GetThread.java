e50f34ced0a03a11dc94968274a07d6b56f63edc
FernandoPPrado <fernandopradoifc@gmail.com>
2026-03-24 19:46:26 -0300

    Implementaçao da thread virtual responsável pelo get do servidor local LHM e finder de resultados

---
+++ SISTEMA/src/main/java/com/fernandoprado/lhmagent/threads/GetThread.java
@@ -0,0 +1,47 @@
+package com.fernandoprado.lhmagent.threads;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fernandoprado.lhmagent.client.LhmClient;
+import com.fernandoprado.lhmagent.service.HardwareFinder;
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class GetThread {
+
+
+    public static LhmClient client = Feign.builder()
+            .decoder(new JacksonDecoder())
+            .target(LhmClient.class, "http://localhost:8085");
+
+    public static ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
+
+    public static HardwareFinder hardwareFinder = new HardwareFinder();
+
+    public void getLHMInfo(Map<String, String> mapaPath, AtomicBoolean atomicBoolean) {
+
+
+        executor.submit(() -> {
+
+            try {
+
+                JsonNode jsonNode = client.getHardwareData();
+                System.out.println(hardwareFinder.lerValoresAtuais(jsonNode, mapaPath));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                atomicBoolean.set(false);
+            }
+
+
+        });
+
+
+    }
+
+}
