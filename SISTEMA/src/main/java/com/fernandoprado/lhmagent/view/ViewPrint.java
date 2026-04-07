9a11c99eb417665ed969583dc8d6efc7abdb4b74
FernandoPPrado <fernandopradoifc@gmail.com>
2026-04-06 22:31:19 -0300

    Classe teste de print no terminal, usada fundamentalmente para desenvolvimento

---
+++ SISTEMA/src/main/java/com/fernandoprado/lhmagent/view/ViewPrint.java
@@ -0,0 +1,22 @@
+package com.fernandoprado.lhmagent.view;
+
+import com.fernandoprado.lhmagent.Controller.model.AppEvent;
+
+import java.util.Map;
+import java.util.concurrent.SubmissionPublisher;
+
+public class ViewPrint {
+
+    SubmissionPublisher<AppEvent<?>> app;
+
+    public ViewPrint(SubmissionPublisher<AppEvent<?>> app2) {
+        this.app = app2;
+        app2.consume((appEvent -> {
+
+            if (appEvent.eventType() == AppEvent.EventType.UPDATE) {
+                System.out.println(appEvent.payload());
+            }
+
+        }));
+    }
+}
